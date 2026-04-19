bc7dce29cea9ad9a69f4a2e099e2d1d9c175d709
shibitha028 <shibu@karvatechnologies.com>
2026-04-19 13:46:59 +0530

    Add npmrc and fix API URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({ 
-  baseURL: 'https://sla-monitor-backend.onrender.com/api' 
+  baseURL: 'http://localhost:5000/api' 
 });
 
 API.interceptors.request.use((config) => {
